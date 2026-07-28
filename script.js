005dc59fcb13bdc8e8f7a97b9305048e67cd9a88
Thirishika <thirishika9218@mountzion.ac.in>
2026-07-28 13:50:24 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 // ===============================
+// Hamburger Menu
+// ===============================
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+});
+// ===============================
 // Typing Animation
 // ===============================
 
